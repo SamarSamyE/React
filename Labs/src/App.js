75ca7b44698a4a2916976d509baf57cd2a8c1449
Samar Samy <samarsamy4844@gmail.com>
2023-05-17 18:58:35 +0300

    day3

--- Labs/src/App.js
+++ Labs/src/App.js
@@ -2,12 +2,16 @@
 // import SliderClass from "./Components/day1/slider";
 // import { Component } from "react";
 import { BrowserRouter,Route, Routes } from "react-router-dom";
-import Header from "./Components/day2/Components/header";
-import Home from "./Components/day2/Components/home";
-import Details from "./Components/day2/Components/details";
-import Profile from "./Components/day2/Components/profile";
+// import Header from "./Components/day2/Components/header";
+// import Home from "./Components/day2/Components/home";
+// import Details from "./Components/day2/Components/details";
+// import Profile from "./Components/day2/Components/profile";
 import Error from "./Components/day2/Components/error";
+import Home from "./Components/day3/home";
+import Singers from "./Components/day3/singers";
+import SingerDetails from "./Components/day3/singerDetails";
 import './style.css'
+//#region day1
 // function App(){
 //     return (
 //         <div>
@@ -17,24 +21,43 @@ import './style.css'
 //         </div>
 //     )
 // }
-
+//#endregion
+//#region day2
+// function App(){
+//   return(
+//     <div>
+//       <BrowserRouter>
+//       <div className="header">
+//         <Header/>
+//       </div>
+//           <Routes>
+//               <Route path="/" element={<Home/>}/>
+//               <Route path="/home" element={<Home/>}/>
+//               <Route path="/students/:id" element={<Details/>}/>
+//               <Route path="/profile" element={<Profile/>}/>
+//               <Route path="*" element={<Error/>}/>
+//           </Routes>
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
+//#endregion
+//#region day3
 function App(){
   return(
     <div>
       <BrowserRouter>
-      <div className="header">
-        <Header/>
-      </div>
           <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/home" element={<Home/>}/>
-              <Route path="/students/:id" element={<Details/>}/>
-              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/singers/:id" element={<SingerDetails/>}/>
+              <Route path="/singers" element={<Singers/>}/>
               <Route path="*" element={<Error/>}/>
           </Routes>
       </BrowserRouter>
+
     </div>
   )
 }
-
+//#endregion
 export default App;
